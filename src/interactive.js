4430e52fb739af846d2e0eeab3b9cd4f2a89208a
rdnrn <rodina.arina00@gmail.com>
2021-12-06 20:23:24 +0300

    Add custom checkbox

---
+++ src/interactive.js
@@ -0,0 +1,11 @@
+import { save, tasks } from './localStorage.js';
+import { render } from './renderTask.js';
+
+export default function checkCompleted(e) {
+  if (e.target.tagName.toLowerCase() === 'input') {
+    const selectedTask = tasks.find((task) => task.id === e.target.id);
+    selectedTask.complete = e.target.checked;
+    save();
+    render();
+  }
+}
