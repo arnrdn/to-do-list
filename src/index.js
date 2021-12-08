be6a570de62f8809b7feb60b3f238f8c2b8f8d0a
rdnrn <rodina.arina00@gmail.com>
2021-12-08 17:30:56 +0300

    I gave up

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {
 } from './addRemove.js';
 import { checkCompleted } from './interactive.js';
 import removeAllCheckedRender from './removeAll.js';
-import { save, tasks } from './localStorage.js';
+import { tasks } from './localStorage.js';
 
 window.addEventListener('DOMContentLoaded', render(tasks));
 
@@ -17,18 +17,6 @@ tasksContainer.addEventListener('change', checkCompleted);
 
 tasksContainer.addEventListener('click', (e) => {
   focus(e);
-});
-
-const bucket = document.getElementsByClassName('delete');
-const checkbox = document.getElementsByClassName('custom-checkbox')
-const unorderedList = document.querySelector('[data-tasks]')
-unorderedList.addEventListener('click', (e) => {
-  if (e.target.tagName.toLowerCase() === 'i') {
-    tasks.forEach((task) => {
-      console.log(task);
-    })
-  }
-
 });
 
 clearCompletedTasksButton.addEventListener('click', removeAllCheckedRender);
